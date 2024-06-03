ceef4a2adb92f8f43818be3c7d3064048aa6cdf9
hvihvi <hugo.vinet@gmail.com>
2024-06-03 10:32:13 +0200

    give python path

--- init.lua
+++ init.lua
@@ -578,7 +578,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -918,6 +918,14 @@ require('lazy').setup({
     },
   },
 })
+
+require('lspconfig').pyright.setup {
+  settings = {
+    python = {
+      pythonPath = '/Users/hvi/github.com/Revyze/Revyze/backend_flask/.venv/bin/python3.9',
+    },
+  },
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
