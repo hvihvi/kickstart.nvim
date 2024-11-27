8d401836bd68a280bd71a79bcbc0d564fbb290fa
hvihvi <hugo.vinet@gmail.com>
2024-11-27 20:00:18 +0100

    change copilot accept to tab

--- init.lua
+++ init.lua
@@ -1057,7 +1057,7 @@ require('lazy').setup({
           hide_during_completion = true,
           debounce = 75,
           keymap = {
-            accept = '<S-Space>',
+            accept = '<Tab>',
             accept_word = false,
             accept_line = false,
             next = '<C-j>',
