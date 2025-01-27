f176182fc80c8a1b67f538eaae75344cb5e6acff
hvihvi <hugo.vinet@gmail.com>
2025-01-27 10:30:38 +0100

    setup neovide

--- init.lua
+++ init.lua
@@ -85,15 +85,15 @@ P.S. You can delete this when you're done too. It's your config now! :)
 
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
-  -- Helper function for transparency formatting
-  local alpha = function()
-    return string.format('%x', math.floor(255 * vim.g.transparency or 0.8))
-  end
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 0.0
-  vim.g.transparency = 0.8
-  vim.g.neovide_background_color = '#0f1117' .. alpha()
-  vim.g.neovide_window_blurred = true
+  -- Allow copy/paste
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+  vim.cmd 'cd ~/github.com/Revyze/Revyze'
 end
 
 -- Set <space> as the leader key
