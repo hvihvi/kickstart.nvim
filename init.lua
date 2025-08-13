891a4c77b20a058826c60c95644b8908ea12334c
hvihvi <hugo.vinet@gmail.com>
2025-08-13 16:51:15 +0200

    smart telescope

--- init.lua
+++ init.lua
@@ -432,11 +432,9 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          path_display = { 'smart' },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
